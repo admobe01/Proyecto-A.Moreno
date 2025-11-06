c58b9c0d795f60511780ea4d9256e4eb1275dc2b
Adrben <admobe@alumnatflorida.es>
2025-11-06 11:10:53 +0100

    Proyecto acabado

---
+++ proyecto1.php
@@ -0,0 +1,357 @@
+<?php
+
+
+$capraMajoris = [
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', 'C', '0', 'C', '0', '0', 'C', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', 'C', '0', '0', '0', '0', 'C', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'C', 'C', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'C', '0', '0', 'C', 'C', 'C', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '0', '0', '0', 'C', '0', 'C', 'C', '0', '0', '0', '0', '0', '0', '0', 'C', '0', '0', '0', 'C', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '~', '0', '0', 'C', '0', '0', '0', 'C', '0', '0', '0', '0', '0', '0', 'C', '0', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', 'C', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'C', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', '0', 'C', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~'],
+    ['~', '0', '0', '0', '0', '0', '0', '0', '0', 'C', '0', '0', '0', 'C', '0', '0', '0', '0', '0', 'C', '0', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', '0', 'C', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'C', 'C', 'C', '0', '0', 'C', '0', '0', '0', '~', '~', '~'],
+    ['~', '~', '~', '~', '0', 'C', 'C', '0', '0', 'C', '0', '0', '0', 'C', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '0', 'C', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'C', '0', '0', '0', '0', '0', 'C', '0', 'C', '0', '0', '0', '~'],
+    ['~', '~', '~', '0', 'C', '0', '0', '0', '0', '0', '0', '0', '0', 'C', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'C', '0', '0', 'C', '0', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '0', '0', '0', '0', 'C', '0', '0', '0', '0', 'C', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'C', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'C', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~']
+];
+
+// Coordenadas [fila, columna] de los impactos de queso
+$impacts0 = [
+    [20, 4],
+    [2, 13],
+    [13, 12],
+    [3, 17],
+    [2, 13],
+    [5, 19],
+    [6, 18],
+    [5, 2],
+    [20, 13],
+    [9, 7],
+    [5, 9],
+    [15, 16],
+    [16, 13],
+    [16, 9],
+    [16, 0],
+    [3, 19],
+    [19, 8],
+    [1, 16],
+    [18, 4],
+    [21, 23],
+    [7, 17],
+    [22, 15],
+    [21, 6],
+    [14, 8],
+    [12, 23],
+    [7, 7],
+    [22, 4],
+    [3, 21],
+    [2, 3],
+    [8, 11],
+    [0, 4],
+    [7, 21],
+    [11, 4],
+    [7, 15],
+    [6, 17],
+    [5, 19],
+    [4, 19],
+    [4, 7],
+    [23, 21],
+    [15, 20],
+    [2, 9],
+    [21, 2],
+    [1, 13],
+    [7, 10],
+    [21, 5],
+    [13, 17],
+    [2, 14],
+    [11, 14],
+    [22, 17],
+    [18, 22],
+    [2, 23],
+    [3, 1],
+    [18, 6],
+    [17, 12],
+    [18, 18],
+    [20, 2],
+    [3, 14],
+    [11, 21],
+    [6, 5],
+    [6, 2],
+    [12, 23],
+    [18, 18],
+    [21, 6],
+    [10, 12],
+    [5, 4],
+    [16, 19],
+    [8, 10],
+    [12, 21],
+    [15, 1],
+    [20, 14],
+    [3, 20],
+    [6, 19],
+    [20, 13],
+    [15, 4],
+    [10, 2],
+    [5, 16],
+    [20, 1],
+    [12, 13],
+    [11, 5],
+    [12, 14],
+    [8, 3],
+    [6, 8],
+    [19, 7],
+    [16, 9],
+    [13, 20],
+    [3, 5],
+    [1, 0],
+    [20, 14],
+    [12, 21],
+    [12, 16],
+    [15, 7],
+    [9, 1],
+    [1, 18],
+    [20, 6],
+    [8, 6],
+    [22, 1],
+    [10, 22],
+    [19, 19],
+    [7, 16],
+    [8, 8]
+];
+
+$mapa2mod = [];
+
+function difEjer(){//esta funcion es solo para poner separaciones en pantalla
+
+$k=50;
+$return = "";
+$return .= "<br><br>"; 
+    for($z=0;$z<$k;$z++){
+
+        $return .= "/";
+}
+
+$return .= "<br><br>"; 
+
+return $return;
+}
+
+/////////////////////////////////////////////////
+echo "Ejercicio 1<br>";
+function mapa1($mapa){
+
+    $resultado="";
+
+foreach ($mapa as $datos){
+
+    foreach($datos as $data){
+
+        $resultado .= $data;
+    }
+
+
+
+    $resultado .= "<br>";
+}
+return $resultado;
+}
+
+echo mapa1($capraMajoris);
+
+echo difEjer();
+
+/////////////////////
+////////////////////////////////////////////
+
+echo "Ejercicio 2<br>";
+
+
+function mapa2($mapa,$impacts){
+
+     for($i=0;$i<count($impacts);$i++){
+
+            if($mapa[$impacts[$i][0]][$impacts[$i][1]] ==  "C"){
+
+                $mapa[$impacts[$i][0]][$impacts[$i][1]] = "Q";
+
+            }
+    }
+return $mapa;
+}
+
+$mapa2mod = mapa2($capraMajoris,$impacts0);
+
+foreach ($mapa2mod as $datos){
+
+    foreach($datos as $data){
+
+        echo $data;
+    }
+    
+    echo"<br >";
+}
+
+echo difEjer();
+
+////////////////
+
+echo "Ejercicio 3<br>";
+
+    function mapa3($mapa){
+
+    $contador=0;
+
+    foreach($mapa as $datos){
+
+        foreach($datos as $data){
+
+            if($data == "Q"){
+
+                $contador++;
+
+            }
+
+        }
+    }
+
+    
+    $contador = $contador*3000;
+    $litros = $contador * 0.01;
+    return "<br>Cabras afectadas: " . $contador . "<br> Litros de desorodante anticheddar: " . $litros . " L";
+}
+
+echo mapa3($mapa2mod);
+echo difEjer();
+
+///////////////////////////////////////////////
+
+echo "Ejercicio 4 <br>";
+
+
+
+function mapa4($mapa, $impacts){
+
+     for($i=0;$i<count($impacts);$i++){
+
+        if($mapa[$impacts[$i][0]][$impacts[$i][1]] ==  "0") {
+
+                $mapa[$impacts[$i][0]][$impacts[$i][1]] = "X";
+
+            }elseif($mapa[$impacts[$i][0]][$impacts[$i][1]] ==  "~") {
+
+                $mapa[$impacts[$i][0]][$impacts[$i][1]] = "S";
+            }
+        }
+return $mapa;
+}
+
+$mapa4mod = mapa4($mapa2mod, $impacts0);
+
+foreach ($mapa4mod as $datos) {
+    foreach ($datos as $data) {
+        echo $data;
+    }
+    echo "<br>";
+}
+
+echo difEjer();
+/////////////////////////////////////////////
+echo "Ejercicio 5";
+
+
+function ejer5($mapa){
+$contadorX = 0;
+$contadorQ = 0;
+    foreach ($mapa as $datos){
+        foreach($datos as $data){
+
+            if($data == "X"){
+
+                $contadorX++;
+            }elseif($data == "Q"){
+
+                $contadorQ++;
+            }
+
+        }
+    }
+    $rocasCoste = $contadorX * 75000;
+    $cabrasCoste = $contadorQ * 25000;
+
+    $dañototal = $rocasCoste + $cabrasCoste;
+
+    return $dañototal;
+}
+
+echo "<br><br>Entre la reparacion al daño del terreno rocoso y la desinfeccion de 
+desquesoficacion de las cabras la suma ascenderia a " . ejer5($mapa4mod) . " €";
+echo difEjer();
+//////////////////////////////////////////////////
+
+echo "Ejercicio 6";
+
+function atmosfera($mapa){
+$atm = 0;
+    foreach ($mapa as $data){
+        foreach ($data as $datos){
+            if ($datos == "~" or $datos == "S"){
+
+                $atm++;
+            }
+        }
+    }
+
+    return $atm;
+}
+
+function pececillos($mapa){
+
+$pez = 0;
+    foreach ($mapa as $data){
+        foreach ($data as $datos){
+            if ($datos == "S"){
+
+                $pez++;
+            }
+        }
+    }
+    return $pez;
+}
+
+echo "<br><br>La cantidad de atmosfera es de " . atmosfera($mapa4mod) . " Km3,
+en cambio la atmosfera impactada es de " . pececillos($mapa4mod) . " km3";
+
+echo difEjer();
+///////////////////////////////
+
+echo "Ejercicio 7";
+function recaudacion($datitos){
+
+    $peces = 1000 * 1000;
+
+    $porcAfect = pececillos($datitos)/atmosfera($datitos);
+    $kgPeces = $porcAfect * $peces;
+    return round($kgPeces * 7);
+}
+
+$diferencia = ejer5($mapa4mod) - recaudacion($mapa4mod);
+
+echo "<br><br>La cantidad de dinero recaudado gracias a la venta de peces gaseosos es de " . recaudacion($mapa4mod) . " € "
+. "<br><br> Por lo que los daños netos teniendo en cuenta la recaudacion es de $diferencia €" ;
+
+
+
+?>
